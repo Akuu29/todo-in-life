f49dd776073f95a108a379e00d98d379b9a050ee
Akuu29 <owner.akiu0529@gmail.com>
2022-05-21 07:25:08 +0900

    Created fn login in main_scopes.rs && Created struct for validation of login form

--- src/scopes/main_scopes.rs
+++ src/scopes/main_scopes.rs
@@ -4,11 +4,10 @@ use actix_identity::Identity;
 use tera::{Tera, Context};
 use diesel::prelude::*;
 use validator::Validate;
-// use argon2::Argon2;
-// use argon2::password_hash::rand_core::OsRng;
-// use argon2::password_hash::{PasswordHash, PasswordHasher, PasswordVerifier, SaltString};
-use crate::users::{NewUser};
-use crate::users::validate_form::{SignupData};
+use argon2::Argon2;
+use argon2::password_hash::{PasswordHash, PasswordVerifier};
+use crate::users::{NewUser, User};
+use crate::users::validate_form::{SignupData, LoginData};
 use crate::Pool;
 use crate::schema::users;
 // use crate::manage_cookie::{get_cookie, set_cookie};
@@ -18,6 +17,7 @@ pub fn get_scope() -> Scope {
         .service(index)
         .service(app)
         .service(render_login)
+        .service(login)
         .service(render_signup)
         .service(signup)
         .service(logout)
@@ -64,6 +64,56 @@ async fn render_login(identitiy: Identity, tmpl: Data<Tera>) -> impl Responder {
     HttpResponse::Ok()
         .content_type("text/html")
         .body(response_body)
+}
+
+#[post("/login")]
+async fn login(identity: Identity, pool: Pool, form_data: Form<LoginData>) -> impl Responder {
+    // すでにサインアップまたはログイン済みの場合、早期リターン
+    if identity.identity().is_some() {
+        return HttpResponse::Found().append_header(("location", "/")).finish();
+    }
+
+    // バリデーション
+    if let Err(validation_errors) = form_data.validate() {
+// TODO クライアント側でエラー内容をどのように受け取るか
+        return HttpResponse::Found()
+            .append_header(("location", "/login"))
+            .finish()
+    }
+
+    let db_connection = pool.get().expect("Failed getting db connection");
+
+    let user = users::table
+        .filter(users::username.eq(&form_data.username))
+        .first::<User>(&db_connection);
+
+    match user {
+        Ok(user) => {
+            // 入力されたパスワードのハッシュ値を保存されているハッシュ値と比較
+            let parsed_hash = PasswordHash::new(&user.password).unwrap();
+            let is_match = Argon2::default().verify_password(form_data.password.as_bytes(), &parsed_hash);
+            match is_match {
+                Ok(_) => {
+                    // cookieにID保存
+                    identity.remember(user.username.clone());
+// TODO クライアント側でcookie管理
+                    HttpResponse::Found()
+                        .append_header(("location", "/app"))
+                        .finish()
+                }
+                Err(_) => {
+                    HttpResponse::Found()
+                        .append_header(("location", "/login"))
+                        .finish()
+                }
+            }
+        }
+        Err(_) => {
+            HttpResponse::Found()
+                .append_header(("location", "/login"))
+                .finish()
+        }
+    }
 }
 
 #[get("/signup")]
--- src/users/validate_form.rs
+++ src/users/validate_form.rs
@@ -19,3 +19,18 @@ pub struct SignupData {
     pub password: String,
 }
 
+#[derive(Debug, Validate, Deserialize)]
+pub struct LoginData {
+    #[validate(length(
+        min = 4,
+        max = 32,
+        message = "Username must be between 4 and 32 characters"
+    ))]
+    pub username: String,
+    #[validate(length(
+        min = 8,
+        max = 32,
+        message = "Password must be between 8 and 32 characters"
+    ))]
+    pub password: String,
+}
