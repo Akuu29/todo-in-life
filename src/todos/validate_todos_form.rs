014ec57479c0bd13b6bb9a6ee33ec2beafb3dd7a
Akuu29 <owner.akiu0529@gmail.com>
2022-05-23 06:28:58 +0900

    Created validate_todos_form.rs Struct for form data validation.

---
+++ src/todos/validate_todos_form.rs
@@ -0,0 +1,37 @@
+use serde::Deserialize;
+use validator::{Validate, ValidationError};
+use chrono::{Utc};
+use crate::{convert_to_date};
+
+#[derive(Debug, Validate, Deserialize)]
+pub struct TodoData {
+    #[validate(length(
+        min = 1,
+        max = 50,
+        message = "Title must be between 1 and 50 characters"
+    ))]
+    pub title: String,
+    #[validate(length(
+        max = 300,
+        message = "Category must be 300 characters or less"
+    ))]
+    pub content: Option<String>,
+    pub category: Option<String>,
+    #[validate(
+        length(min = 8, max = 8, message = ""),
+        custom = "validate_past_date"
+    )]
+    pub date_limit: Option<String>,
+}
+
+// date_limitに過去日が入力された場合、NGとする
+fn validate_past_date(date_limit: &str) -> Result<(), ValidationError>{
+    let date = convert_to_date(date_limit);
+    let today = Utc::today().naive_utc();
+    let duration = date - today;
+    if duration.num_days() < 0 {
+        return Err(ValidationError::new("past_date"));
+    }
+
+    Ok(())
+}
