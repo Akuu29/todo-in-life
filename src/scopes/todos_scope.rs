d40b2775bac1bf8045e04255b2a110ee05f4ef6b
Akuu29 <owner.akiu0529@gmail.com>
2022-05-24 01:21:41 +0900

    Created fn "update" in todos_scope.rs

--- src/scopes/todos_scope.rs
+++ src/scopes/todos_scope.rs
@@ -1,19 +1,21 @@
-use actix_web::{get, post, Scope, Responder, HttpResponse};
+use actix_web::{get, post, put,Scope, Responder, HttpResponse};
 use actix_web::web::{self, Form};
 use actix_identity::Identity;
 use diesel::prelude::*;
 use validator::Validate;
 use serde_json::json;
 use crate::Pool;
-use crate::todos::validate_todos_form::TodoData;
+use crate::todos::validate_todos_form::{TodoData, EditTodoData};
 use crate::todos::{NewTodo, Todo};
 use crate::schema::{users, todos};
 use crate::users::User;
+use crate::convert_to_date;
 
 pub fn get_scope() -> Scope {
     web::scope("/api")
         .service(get)
         .service(create)
+        .service(update)
 }
 
 #[get("/todos")]
@@ -81,5 +83,59 @@ pub async fn create(identitiy: Identity, pool: Pool, todo_data: Form<TodoData>) 
         // Ok(Ok(_)) => HttpResponse::Created().json(),
         Ok(Ok(_)) => HttpResponse::Created().json(json!({"status": "201"})),
         _ => HttpResponse::InternalServerError().finish(), // 500
+    }
+}
+
+#[put("/todos")]
+pub async fn update(identity: Identity, pool: Pool, todo_data: Form<EditTodoData>) -> impl Responder {
+    // 未ログインの場合、早期リターン
+    if identity.identity().is_none() {
+        return HttpResponse::Unauthorized().finish(); // 401
+    }
+
+    // バリデーション
+    if let Err(_validation_errors) = todo_data.validate() {
+        return HttpResponse::UnprocessableEntity().finish(); // 422
+    }
+
+    let db_connection = pool.get().expect("Failed getting db connection");
+
+    let user_id = users::table
+        .filter(users::username.eq(identity.identity().unwrap()))
+        .first::<User>(&db_connection)
+        .unwrap()
+        .id;
+
+    // todoからuser_id取得
+    let get_todo_result = todos::table
+        .filter(todos::id.eq(todo_data.id.clone()))
+        .first::<Todo>(&db_connection);
+    let todo_user_id = match get_todo_result {
+        Ok(todo) => todo.user_id,
+        Err(_) => return HttpResponse::Forbidden().finish(), // 403
+    };
+
+    // user_id と todo_user_idが一致しない場合エラー
+    if user_id != todo_user_id {
+        return HttpResponse::Forbidden().finish(); // 403
+    }
+
+    // todo_data.date_limitをStringからDate型に変換
+    let convert_date_limit = todo_data.date_limit.clone()
+        .map(|date| convert_to_date(&date));
+
+    let update_result = web::block(move || {
+        diesel::update(todos::table.filter(todos::id.eq(&todo_data.id).and(todos::user_id.eq(user_id))))
+            .set((
+                todos::title.eq(todo_data.title.clone()),
+                todos::content.eq(todo_data.content.clone()),
+                todos::date_limit.eq(convert_date_limit),
+            ))
+            .execute(&db_connection)
+    }).await;
+
+    match update_result {
+        Ok(Ok(_)) => HttpResponse::Ok().json(json!({"status": "200"})),
+        _ => HttpResponse::InternalServerError().finish()
     }
 }
