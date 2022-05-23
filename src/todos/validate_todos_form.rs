403af618f7ec0fc9a2c36f53663e70500974be88
Akuu29 <owner.akiu0529@gmail.com>
2022-05-24 01:19:32 +0900

    Created EditTodoData Struct for validation.

--- src/todos/validate_todos_form.rs
+++ src/todos/validate_todos_form.rs
@@ -24,6 +24,28 @@ pub struct TodoData {
     pub date_limit: Option<String>,
 }
 
+#[derive(Debug, Validate, Deserialize)]
+pub struct EditTodoData {
+    pub id: String,
+    #[validate(length(
+        min = 1,
+        max = 50,
+        message = "Title must be between 1 and 50 characters"
+    ))]
+    pub title: String,
+    #[validate(length(
+        max = 300,
+        message = "Category must be 300 characters or less"
+    ))]
+    pub content: Option<String>,
+    pub category: Option<String>,
+    #[validate(
+        length(min = 8, max = 8, message = ""),
+        custom = "validate_past_date"
+    )]
+    pub date_limit: Option<String>,
+}
+
 // date_limitに過去日が入力された場合、NGとする
 fn validate_past_date(date_limit: &str) -> Result<(), ValidationError>{
     let date = convert_to_date(date_limit);
