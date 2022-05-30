f7de2caaca5bfdb88ef54c2cf30b42b8cdae257a
Akuu29 <owner.akiu0529@gmail.com>
2022-05-30 16:40:48 +0900

    Deleted GlobalTodoForm.tsx && Write css in AppForm.tsx

--- static/js/components/App/AppForm.tsx
+++ static/js/components/App/AppForm.tsx
@@ -1,7 +1,72 @@
+import { css } from "@emotion/react";
 import React, {useState} from "react";
-import GlobalTodoForm from "../../styles/GlobalTodoForm";
 
-const AppForm: React.FC = () => {
+const todoFormWrapper = css({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  position: "fixed",
+  top: 0,
+  left: 0,
+  width: "100%",
+  height: "100%",
+  backgroundColor: "rgba(0,0,0,0.5)",
+});
+
+const todoForm = css({
+  zIndex: 2,
+  height: "70%",
+  width: "30%",
+  padding: "3em",
+  background: "#ffffff",
+  border: "solid",
+  borderRadius: 10,
+});
+
+const titleWrapper = css({
+  textAlign: "center",
+  marginBottom: 20, 
+  "& h1": {
+    fontSize: 30,
+    borderBottom: "solid",
+    paddingBottom: 5,
+  }
+});
+
+const todoFormContent = css({
+  width: 400,
+  marginBottom: 20,
+  "& input": {
+    width: 400,
+  },
+  "& textarea": {
+    width: 400,
+    height: 150,
+  },
+  "& select": {
+    width: 150,
+    display: "block",
+  },
+  ".inputDate": {
+    width: 150,
+    display: "block",
+  }
+});
+
+const btnWrapper = css({
+  textAlign: "center",
+  paddingTop: 20,
+  "& input": {
+    width: "100%",
+    height: 30,
+    backgroundColor: "#000000",
+    color: "#ffffff",
+    borderRadius: 6,
+    "&:hover": {
+      backgroundColor: "#2b2b2b",
+    }
+  }
+});
   const [todo, setTodo] = useState({
     title: "",
     content: "",
@@ -14,13 +79,12 @@ const AppForm: React.FC = () => {
   }
 
   return (
-    <div className="todoFormWrapper">
-      <GlobalTodoForm />
-      <form className="todoForm">
-        <div className="titleWrapper">
+    <div css={[todoFormWrapper, isRender]}>
+      <form css={todoForm}>
+        <div css={titleWrapper}>
           <h1>POST</h1>
         </div>
-        <div className="todoFormContent">
+        <div css={todoFormContent}>
           <label>Title</label>
           <input
             type="text"
@@ -28,13 +92,13 @@ const AppForm: React.FC = () => {
             required
             onChange={handleChange} />
         </div>
-        <div className="todoFormContent">
+        <div css={todoFormContent}>
           <label>Content</label>
           <textarea
             name="content"
             onChange={handleChange} />
         </div>
-        <div className="todoFormContent">
+        <div css={todoFormContent}>
           <label>Category</label>
           <select
             name="category"
@@ -45,7 +109,7 @@ const AppForm: React.FC = () => {
             <option value="long">long</option>
           </select>
         </div>
-        <div className="todoFormContent">
+        <div css={todoFormContent}>
           <label>Limit Date</label>
           <input
             className="inputDate"
@@ -53,7 +117,7 @@ const AppForm: React.FC = () => {
             name="date_limit"
             onChange={handleChange} />
         </div>
-        <div className="btnWrapper">
+        <div css={btnWrapper}>
           <input type="submit" value="POST" />
         </div>
       </form>
