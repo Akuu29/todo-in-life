5d44de22d5cef505d443e196a36ae86820cde1f2
Akuu29 <owner.akiu0529@gmail.com>
2022-06-01 23:49:22 +0900

    Created pagenation

--- static/js/components/App/AppTodos.tsx
+++ static/js/components/App/AppTodos.tsx
@@ -13,87 +13,86 @@ const AppTodosWrapper = css({
   justifyContent: "space-around",
 });
 
+export interface Todos {
+  [key: string]: Array<Array<CreatedTodo>>;
+}
+
 const AppTodos: FC<{setCreateForm: SetCreateForm}> = ({setCreateForm}) => {
-  const [todos, setTodos] = useState<Array<CreatedTodo>>([]);
+  const [todos, setTodos] = useState<Todos>({
+    "short": [],
+    "medium": [],
+    "long": [],
+    "completed": [],
+  });
+
+  // カテゴリごとにstateを持つ
+  // const [todosShort, setTodosShort] = useState<Todos>([]);
+  // const [todosMedium, setTodosMedium] = useState<Todos>([]);
+  // const [todosLong, setTodosLong] = useState<Todos>([]);
+  // const [todosCompleted, setTodosCompleted] = useState<Todos>([]);
+
+  // それぞれのカテゴリ内でのページを保持
+  const [pageShort, setPageShort] = useState(0);
+  const [pageMedium, setPageMedium] = useState(0);
+  const [pageLong, setPageLong] = useState(0);
+  const [pageCompleted, setPageCompleted] =useState(0);
 
   useEffect(() => {
     const get_todos = () => {
-      const sampleTodos = [
-        {
-          id: "57894y82935",
-          title: "おはようございます。",
-          content: "気持ちのいい朝ですね",
-          category: "completed",
-          date_limit: "2022/09/01",
-          done: true,
-          date_created: "2022/05/28",
-        },
-        {
-          id: "57894y829aaaa",
-          title: "こんにちは。",
-          content: "気持ちのいい昼ですね",
-          category: "completed",
-          date_limit: "2022/09/01",
-          done: false,
-          date_created: "2022/05/28",
-        },
-        {
-          id: "57894y82922aa",
-          title: "こんにちは。",
-          content: "気持ちのいい昼ですね",
-          category: "short",
-          date_limit: "2022/09/01",
-          done: false,
-          date_created: "2022/05/28",
-        },
-        {
-          id: "57894y82wett9gfs",
-          title: "こんばんは。",
-          content: "気持ちのいいよるですね",
-          category: "short",
-          date_limit: "2022/09/01",
-          done: false,
-          date_created: "2022/05/28",
-        },
-        {
-          id: "57894yewet829gfs",
-          title: "こんばんは。",
-          content: "気持ちのいいよるですね",
-          category: "medium",
-          date_limit: "2022/09/01",
-          done: false,
-          date_created: "2022/05/28",
-        },
-        {
-          id: "57g94y82935",
-          title: "おはようございます。",
-          content: "気持ちのいい朝ですね",
-          category: "long",
-          date_limit: "2022/09/01",
-          done: true,
-          date_created: "2022/05/28",
-        },
-      ];
 
-      setTodos(sampleTodos);
+      let todosData: Todos = {
+        "short": [],
+        "medium": [],
+        "long": [],
+        "completed": [],
+      };
+
+      // TODO! 後にsampleGetTodos()を削除
+      const todosGetted: Array<CreatedTodo> = SampleGetTodos();
+
+      todosGetted.forEach((todo: CreatedTodo) => {
+        const key: keyof Todos = todo.category;
+        
+        if(!todosData[key].length) {
+          todosData[key].push([]);
+        }
+
+        todosData[key][0].push(todo);
+      });
+
+      // 1pageに6件todoを表示するため、todoが格納されている配列の長さを最大6配列に分割する
+      Object.keys(todosData).forEach((key) => {
+        if(todosData[key][0] && todosData[key][0].length >= 6) {
+          const numOfArr = Math.ceil(todosData[key][0].length / 6);
+
+          let todos = todosData[key].pop();
+          for(let i = 0; i < numOfArr; i++) {
+            todosData[key].push(todos!.slice(i * 6, (i + 1) * 6));
+          }
+        }
+      });
+
+      setTodos(todosData);
     };
 
     get_todos();
   }, []);
 
-  const returnTodosForColumn = (columnName: string) => {
-    return todos
-      .filter((todo) => todo.category == columnName)
-      .map((todo) => (
-        <AppTodo
-          key={todo.id}
-          id={todo.id}
-          title={todo.title}
-          content={todo.content}
-          date_limit={todo.date_limit}
-          date_created={todo.date_created}
-          setTodos={setTodos} />
-      ));
+  const returnTodosForColumn = (columnName: string, page: number) => {
+    if(todos[columnName][page]) {
+      return todos[columnName][page]
+        .map((todo) => (
+          <AppTodo
+            key={todo.id}
+            id={todo.id}
+            title={todo.title}
+            content={todo.content}
+            date_limit={todo.date_limit}
+            date_created={todo.date_created}
+            setTodos={setTodos} />
+        ));
+    }
+
   };
  
   return (
@@ -102,23 +101,35 @@ const AppTodos: FC<{setCreateForm: SetCreateForm}> = ({setCreateForm}) => {
       <DndProvider backend={HTML5Backend}>
         <AppTodosCategoryColumn
           title="short"
-          setCreateForm={setCreateForm} >
-          {returnTodosForColumn("short")}
+          setCreateForm={setCreateForm}
+          currentPage={pageShort}
+          maxPage={todos["short"].length - 1}
+          setPage={setPageShort} >
+          {returnTodosForColumn("short", pageShort)}
         </AppTodosCategoryColumn>
         <AppTodosCategoryColumn
           title="medium"
-          setCreateForm={setCreateForm} >
-          {returnTodosForColumn("medium")}
+          setCreateForm={setCreateForm}
+          currentPage={pageMedium}
+          maxPage={todos["medium"].length - 1}
+          setPage={setPageMedium} >
+          {returnTodosForColumn("medium", pageMedium)}
         </AppTodosCategoryColumn>
         <AppTodosCategoryColumn
           title="long"
-          setCreateForm={setCreateForm} >
-          {returnTodosForColumn("long")}
+          setCreateForm={setCreateForm}
+          currentPage={pageLong}
+          maxPage={todos["long"].length - 1}
+          setPage={setPageLong} >
+          {returnTodosForColumn("long", pageLong)}
         </AppTodosCategoryColumn>
         <AppTodosCategoryColumn
           title="completed"
-          setCreateForm={setCreateForm} >
-          {returnTodosForColumn("completed")}
+          setCreateForm={setCreateForm}
+          currentPage={pageCompleted}
+          maxPage={todos["completed"].length - 1}
+          setPage={setPageCompleted}>
+          {returnTodosForColumn("completed", pageCompleted)}
         </AppTodosCategoryColumn>
       </DndProvider>
     </div>
--- static/js/components/App/AppTodos/AppTodosCategoryColumn.tsx
+++ static/js/components/App/AppTodos/AppTodosCategoryColumn.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode } from "react";
+import { Dispatch, FC, ReactNode, SetStateAction } from "react";
 import { useDrop } from "react-dnd";
 import { css } from "@emotion/react";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
@@ -14,7 +14,7 @@ const categoryTitle = css({
 
 const todosWrapper = css({
   width: 330,
-  height: 1000,
+  height: 1160,
   border: "solid",
   borderRadius: 8,
 });
@@ -44,14 +44,43 @@ const todoOrderBtn = css({
   }
 });
 
+const pagenation = css({
+  width: "90%",
+  margin: "auto",
+  paddingTop: 5,
+});
+
+const prevBtn = css({
+  cursor: "pointer",
+  "&:hover": {
+    opacity: 0.6,
+  }
+});
+
+const nextBtn = css({
+  float: "right",
+  cursor: "pointer",
+  "&:hover": {
+    opacity: 0.6,
+  }
+});
+
+type SetPage = Dispatch<SetStateAction<number>>;
+
 const AppTodosCategoryColumn: FC<{
     children: ReactNode;
     title: string;
     setCreateForm: SetCreateForm;
+    currentPage: number,
+    maxPage: number,
+    setPage: SetPage;
   }> = ({
     children,
     title,
-    setCreateForm
+    setCreateForm,
+    currentPage,
+    maxPage,
+    setPage
   }) => {
   
   const [, drop] = useDrop({
@@ -59,37 +88,49 @@ const AppTodosCategoryColumn: FC<{
     drop: () => ({name: title}),
   });
 
+  const switchToPrev = () => {
+    setPage(currentPage - 1);
+  }
+
+  const switchToNext = () => {
+    setPage(currentPage + 1);
+  };
+
   return (
     <div>
       <h1 css={categoryTitle}>{title}</h1>
-        <nav css={navigationArea}>
-          <ul>
-          {title !== "completed" &&
-            <li>
-              <Tooltip tooltipType="plusIcon">
-                <a css={displayFormBtn} onClick={() => {setCreateForm([true, title])}}>
-                  <FontAwesomeIcon icon={faPlus} />
-                </a>
-              </Tooltip>
-            </li>}
-            <li>
-              <Tooltip tooltipType="DL">
-                <a css={todoOrderBtn}>
-                  DL
-                </a>
-              </Tooltip>
-            </li>
-            <li>
-              <Tooltip tooltipType="DC">
-                <a css={todoOrderBtn}>
-                  DC
-                </a>
-              </Tooltip>
-            </li>
-          </ul>
-        </nav>
+      <nav css={navigationArea}>
+        <ul>
+        {title !== "completed" &&
+          <li>
+            <Tooltip tooltipType="plusIcon">
+              <a css={displayFormBtn} onClick={() => {setCreateForm([true, title])}}>
+                <FontAwesomeIcon icon={faPlus} />
+              </a>
+            </Tooltip>
+          </li>}
+          <li>
+            <Tooltip tooltipType="DL">
+              <a css={todoOrderBtn}>
+                DL
+              </a>
+            </Tooltip>
+          </li>
+          <li>
+            <Tooltip tooltipType="DC">
+              <a css={todoOrderBtn}>
+                DC
+              </a>
+            </Tooltip>
+          </li>
+        </ul>
+      </nav>
       <div css={todosWrapper} ref={drop}>
         {children}
+      </div>
+      <div css={pagenation}>
+        {currentPage > 0 && <a css={prevBtn} onClick={switchToPrev}>Prev</a>}
+        {currentPage < maxPage && <a css={nextBtn} onClick={switchToNext}>Next</a>}
       </div>
     </div>
   );
