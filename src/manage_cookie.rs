2ed51e90ce60aed35a80b3c9ec3e14b26310c7cc
Akuu29 <owner.akiu0529@gmail.com>
2022-06-07 22:34:45 +0900

    Created fn 'generate_cookie_messages' This function returns initialized messages if messages exist in cookie, and generates and returns new messages if messages don't exist in cookie

--- src/manage_cookie.rs
+++ src/manage_cookie.rs
@@ -1,10 +1,18 @@
 use actix_web::{HttpRequest};
 use actix_web::cookie::Cookie;
 
-// pub fn get_cookie(req: HttpRequest) {
-//     println!("{:?}", req);
-// }
 
-// pub fn set_cookie() {
+pub fn generate_cookie_messages(req: &HttpRequest) -> Cookie {
+    // cookieにmessagesが存在しない場合、新たに生成
+    // 存在した場合、初期化
+    req.cookie("messages").map_or(
+        Cookie::build("messages", "[]")
+            .http_only(false)
+            .finish(),
+        |mut messages| {
+            messages.set_value("[]");
+            messages
+        },
+    )
+}
 
-// }
