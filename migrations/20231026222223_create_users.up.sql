d4264000be7d399e8e1235c5b7d3e8d413d587b1
AKUU <57586401+Akuu29@users.noreply.github.com>
2023-12-02 16:30:14 +0900

    Add: Added sqlx migrations
    * Created sqlx migrations
    * Deleted diesel migrations

--- migrations/20231026222223_create_users.up.sql
+++ migrations/20231026222223_create_users.up.sql
@@ -1,9 +1,8 @@
--- Your SQL goes here
-
 CREATE TABLE users (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    id SERIAL PRIMARY KEY,
     username VARCHAR(32) NOT NULL UNIQUE,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    data_created TIMESTAMP NOT NULL DEFAULT now()
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP NOT NULL DEFAULT now()
 );
---
+++ migrations/20231026222238_create_todos.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE todos (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(150) NOT NULL,
+    content VARCHAR(900),
+    category VARCHAR(18) NOT NULL,
+    date_limit DATE,
+    user_id SERIAL REFERENCES users (id) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP NOT NULL DEFAULT now()
+);
